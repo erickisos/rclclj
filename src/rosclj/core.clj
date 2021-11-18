412fba9b35c73f0dba0bc1f4a09d72d707334069
Erick G. Islas-Osuna <erickisos653@gmail.com>
2021-11-18 06:43:25 -0600

    main structure

---
+++ src/rosclj/core.clj
@@ -0,0 +1,6 @@
+(ns rosclj.core
+  (:require [schema.core :as s]))
+
+(s/defn main
+  []
+  (println "hello world"))
---
+++ test/rosclj/core_test.clj
@@ -0,0 +1,4 @@
+(ns rosclj.core-test
+  (:require [rosclj.core :as sut]
+            [clojure.test :as t]))
+
